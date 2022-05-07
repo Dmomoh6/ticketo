265ef4fe180466bbe29f9a3934c1ffab35d099aa
Emmanuel Momoh <emmamomohs@gmail.com>
2022-05-07 11:01:49 +0100

    final edit

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.baseURL,
   withCredentials: false,
   headers: {
     Accept: "application/json",
